34519f135477d5d3069a51dd581aedb27cd6e70d
Maike Wilms <maikewilms@gmx.net>
2024-06-03 14:23:17 +0200

    Auslagern der ActionListener in Weekplan in eigene Methoden

--- src/Weekplan/Weekplan.java
+++ src/Weekplan/Weekplan.java
@@ -24,59 +24,20 @@ public class Weekplan
     }
     
     /**
-     * Adds the listeners of all components in the UI:
-     * AddButton.actionListener: Opens AddArea and gives it an observer
-     * Label.mouseListener: Opens TopicArea and gives it an observer
-     * SaveButton.actionListener: Saves the topic-values onto the file TopicList.FILENAME
-     * OptionButton.actionListener: Closes window and opens OptionArea
+     * Adds the listeners of all components in the UI
      */
     private void addListener()
     {
-        //listeners for task-options on the left side
-        for (JButton title : _ui.getTitleButtonList())
-        {
-            title.addActionListener(event ->
-            {
-                int taskNumber = _ui.getTitleButtonList().indexOf(title);
-                Task task = _listOfTasks.get(taskNumber);
-                final TaskEditArea area = new TaskEditArea(task, _ui.getMainframe());
-                
-                area.addPropertyChangeListener(evt -> _ui.updateTask(task));
-                area.showUI();
-            });
-        }
-        
-        //listeners for adding values on the right side
-        for (JButton button : _ui.getAddButtonlist())
-        {
-            button.addActionListener(event ->
-            {
-                int topicNumber = _ui.getAddButtonlist().indexOf(button);
-                Task task = _listOfTasks.get(topicNumber);
-                final AddProgressArea area = new AddProgressArea(task, _ui.getMainframe());
-                
-                area.addPropertyChangeListener(evt -> _ui.updateProgress(task));
-                area.showUI();
-            });
-        }
-        
-        
-        //listener for saving
-        _ui.getSaveButton().addActionListener(event -> _listOfTasks.saveTasksOnFile());
-        
+        for (JButton taskTitle : _ui.getTitleButtonList())
+            taskTitle.addActionListener(event -> openTaskEditArea(taskTitle));
+
+        for (JButton addButton : _ui.getAddButtonlist())
+            addButton.addActionListener(event -> openAddProgressArea(addButton));
+
         //listener for options - TODO: Options into a JMenuBar
-        _ui.getOptionButton().addActionListener(event ->
-        {
-            _listOfTasks.saveTasksOnFile();
-            _ui.close();
-            new OptionArea();
-        });
-        
-        _ui.getCloseButton().addActionListener(event ->
-        {
-            _listOfTasks.saveTasksOnFile();
-            _ui.close();
-        });
+        _ui.getOptionButton().addActionListener(event -> openOptionsArea());
+        _ui.getSaveButton().addActionListener(event -> saveTracker());
+        _ui.getCloseButton().addActionListener(event -> closeTracker());
     }
     
     /**
@@ -87,5 +48,60 @@ public class Weekplan
     public void openTaskEdit()
     {
         _ui.openNewTopicMenu();
+    }
+
+    /**
+     * TitleButton.actionListener: Opens TaskEditArea and gives it an observer
+     * @param button
+     */
+    private void openTaskEditArea(JButton button)
+    {
+        int taskNumber = _ui.getTitleButtonList().indexOf(button);
+        Task task = _listOfTasks.get(taskNumber);
+        final TaskEditArea area = new TaskEditArea(task, _ui.getMainframe());
+
+        area.addPropertyChangeListener(evt -> _ui.updateTask(task));
+        area.showUI();
+    }
+
+    /**
+     * AddButton.actionListener: Opens AddProgressArea and gives it an observer
+     * @param button
+     */
+    private void openAddProgressArea(JButton button)
+    {
+        int topicNumber = _ui.getAddButtonlist().indexOf(button);
+        Task task = _listOfTasks.get(topicNumber);
+        final AddProgressArea area = new AddProgressArea(task, _ui.getMainframe());
+
+        area.addPropertyChangeListener(evt -> _ui.updateProgress(task));
+        area.showUI();
+    }
+
+    /**
+     * SaveButton.actionListener: Saves the topic-values onto the file TopicList.FILENAME
+     */
+    private void saveTracker()
+    {
+        _listOfTasks.saveTasksOnFile();
+    }
+
+    /**
+     * OptionButton.actionListener: Closes window and opens OptionArea
+     */
+    private void openOptionsArea()
+    {
+        _listOfTasks.saveTasksOnFile();
+        _ui.close();
+        new OptionArea();
+    }
+
+    /**
+     * CloseButton.actionListener: Saves the Tracker and closes it
+     */
+    private void closeTracker()
+    {
+        _listOfTasks.saveTasksOnFile();
+        _ui.close();
     }
 }
