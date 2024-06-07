985efe341a7c4d6ecf55eacdf4f52b02538abaac
Maike Wilms <maikewilms@gmx.net>
2024-06-07 15:33:07 +0200

    TaskEditArea: Outsourcing of ActionListeners to separate methods

--- src/TaskEditArea/TaskEditArea.java
+++ src/TaskEditArea/TaskEditArea.java
@@ -7,8 +7,8 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 /**
- * Functional class TaskEditArea for the window that allows editing basic values of the given Topic:
- * Name, targetLength
+ * Functional class TaskEditArea for the window that allows editing basic values of the given Task:
+ * Name, targetTime
  */
 public class TaskEditArea
 {
@@ -36,64 +36,73 @@ public class TaskEditArea
     }
     
     /**
-     * Adds the listeners of all components in the UI:
-     * BackButton.actionListener: Closes the UI and returns to the mainScreen
-     * ConfirmButton.actionListener: Closes the UI and saves the temporarily changed values as new values for the task
-     * TaskField.actionListener: If the entry is valid (trimmed and no '#'), it becomes the temporarily new name of the task
-     * TargetTimeField.actionListener: If the entry is valid (int > 0), it becomes the temporarily new targetTime of the task
+     * Adds the listeners of all components in the UI
      */
     private void addListener()
     {
-        _ui.getTaskField().addActionListener(event ->
-        {
-            String input = _ui.getTaskField().getText();
-            if (isValidTaskTitle(input))
-            {
-                _taskName = input;
-                _ui.setTaskLabel(_taskName);
-                _ui.clearTaskField();
-            }
-        });
-        
-        _ui.getTargetTimeField().addActionListener(event ->
-        {
-            String input = _ui.getTargetTimeField().getText();
-            if (isValidTargetTime(input))
-            {
-                _taskTargetTime = Integer.parseInt(input);
-                _ui.setTargetTimeLabel(_taskTargetTime);
-                _ui.clearTargetTimeField();
-            }
-        });
-        
-        _ui.getConfirmButton().addActionListener(event ->
-        {
-            _task.setTitle(_taskName);
-            _task.setTargetTime(_taskTargetTime);
-            confirmChange(1);
-            _ui.close();
-        });
-        
         _ui.getBackButton().addActionListener(event -> _ui.close());
+
+        _ui.getTaskField().addActionListener(event -> changeTaskTitle());
+        _ui.getTargetTimeField().addActionListener(event -> changeTaskTargetTime());
+        _ui.getConfirmButton().addActionListener(event -> submitChanges());
     }
-    
+
     /**
-     * Checks if a given string is valid as a title for a topic:
+     * If the entry is valid, it becomes the temporarily new name of the task
+     */
+    private void changeTaskTitle()
+    {
+        String input = _ui.getTaskField().getText();
+        if (isValidTaskTitle(input))
+        {
+            _taskName = input;
+            _ui.setTaskLabel(_taskName);
+            _ui.clearTaskField();
+        }
+    }
+
+    /**
+     * If the entry is valid, it becomes the temporarily new targetTime of the task
+     */
+    private void changeTaskTargetTime()
+    {
+        String input = _ui.getTargetTimeField().getText();
+        if (isValidTargetTime(input))
+        {
+            _taskTargetTime = Integer.parseInt(input);
+            _ui.setTargetTimeLabel(_taskTargetTime);
+            _ui.clearTargetTimeField();
+        }
+    }
+
+    /**
+     * Closes the UI and saves the temporarily changed values as new values for the task
+     */
+    private void submitChanges()
+    {
+        _task.setTitle(_taskName);
+        _task.setTargetTime(_taskTargetTime);
+        _support.firePropertyChange("Foo", 0, 1);
+        _ui.close();
+    }
+
+    /**
+     * Checks if a given string is valid as a title for a task:
      * Cant begin or end with an empty space or contain an '#'
      *
-     * @param s: String to potentially become the title of a topic
+     * @param s: String to potentially become the title of a task
      * @return result as boolean
      */
     private boolean isValidTaskTitle(String s)
     {
         return s.equals(s.trim()) && !s.contains("#");
     }
-    
+
     /**
      * Checks if a given string is valid as a goal for a task:
      * Has to be a positive integer-value
      *
-     * @param input: String to potentially become the length of a task
+     * @param input: String to potentially become the target time of a task
      * @return result as boolean
      */
     private boolean isValidTargetTime(String input)
@@ -101,14 +110,6 @@ public class TaskEditArea
         return input.matches("\\d+") && !input.equals("0");
     }
 
-    /**
-     * Makes the UI visible (important for the Observer Weekplan)
-     */
-    public void showUI()
-    {
-        _ui.showUI();
-    }
-    
     /**
      * Allows listeners to be added
      *
@@ -118,14 +119,12 @@ public class TaskEditArea
     {
         _support.addPropertyChangeListener(pcl);
     }
-    
+
     /**
-     * Tells the PropertyChangeListeners that a change happens if confirm was clicked
-     *
-     * @param number: the number typed into the textField
+     * Makes the UI visible
      */
-    public void confirmChange(int number)
+    public void showUI()
     {
-        _support.firePropertyChange("Test", 0, number);
+        _ui.showUI(); // important for the observer Weekplan
     }
 }
