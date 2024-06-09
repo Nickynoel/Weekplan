849a71a5220708968c3c5037e19f6c5abd413506
Maike Wilms <maikewilms@gmx.net>
2024-06-09 11:42:34 +0200

    Turning the Comparator from Object + Typecast to Generic (as it should be)

--- src/TaskList/Task/TaskComparatorByProgressInPercent.java
+++ src/TaskList/Task/TaskComparatorByProgressInPercent.java
@@ -6,36 +6,24 @@ import java.util.Comparator;
  * A comparator that sorts tasks by their percentual progress
  */
 
-public class TaskComparatorByProgressInPercent implements Comparator
+public class TaskComparatorByProgressInPercent implements Comparator<Task>
 {
     /**
      * The actual comparison method
-     * @param o1: the first task
-     * @param o2: the second task
+     * @param t1: the first task
+     * @param t2: the second task
      * @return -1,0,1 to show how the two tasks compare via getPercentProgress
      */
     @Override
-    public int compare(Object o1, Object o2)
+    public int compare(Task t1, Task t2)
     {
-        try
+        if (t1.getProgressInPercent()>t2.getProgressInPercent())
         {
-            Task t1 = (Task) o1;
-            Task t2 = (Task) o2;
-            if (t1.getProgressInPercent()>t2.getProgressInPercent())
-            {
-                return 1;
-            }
-            else if (t1.getProgressInPercent()<t2.getProgressInPercent())
-            {
-                return -1;
-            }
-            
+            return 1;
         }
-        catch (ClassCastException e)
+        else if (t1.getProgressInPercent()<t2.getProgressInPercent())
         {
-            javax.swing.JOptionPane.showMessageDialog
-                    (new javax.swing.JFrame(),"This comparator is only meant to be used " +
-                            "for comparison between two elements of the class Task");
+            return -1;
         }
         return 0;
     }
